c117efbefdaa489df36e3324d8491d496095cf11
datchkh <datachokheli5@gmail.com>
2025-09-27 18:33:43 -0400

    added community cards. Needs winner evaluation

--- poker.py
+++ poker.py
@@ -1,14 +1,8 @@
 import random
 
-card_set = ["2 Clubs", "3 Clubs", "4 Clubs", "5 Clubs", "6 Clubs",
-            "7 Clubs", "8 Clubs", "9 Clubs", "10 Clubs",
-            "2 Hearts", "3 Hearts", "4 Hearts", "5 Hearts", "6 Hearts",
-            "7 Hearts", "8 Hearts", "9 Hearts", "10 Hearts",
-            "2 Spades", "3 Spades", "4 Spades", "5 Spades", "6 Spades",
-            "7 Spades", "8 Spades", "9 Spades", "10 Spades",
-            "2 Diamonds", "3 Diamonds", "4 Diamonds", "5 Diamonds", "6 Diamonds",
-            "7 Diamonds", "8 Diamonds", "9 Diamonds", "10 Diamonds",
-            "Ace of Clubs", "Ace of Hearts", "Ace of Spades", "Ace of Diamonds"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+deck = [f"{rank} of {suit}" for rank in ranks for suit in suits]
 
 players = {
     "Player 1": [],
@@ -17,12 +11,28 @@ players = {
     "Player 4": [],
 }
 
-random.shuffle(card_set)
+random.shuffle(deck)
 
 for _ in range(2):
     for player in players:
-        card = card_set.pop()
+        card = deck.pop()
         players[player].append(card)
+
+community_card = []
+
+deck.pop()
+for _ in range(3):
+    community_card.append(deck.pop())
+
+deck.pop()
+community_card.append(deck.pop())
+
+deck.pop()
+community_card.append(deck.pop())
 
 for player, hand in players.items():
     print(f"{player}: {hand}")
+
+print("")
+print("Community cards:")
+print(community_card)
